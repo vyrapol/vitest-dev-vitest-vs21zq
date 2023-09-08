227ca3acfe72c59c624c8bbd0c2ea170fc501467
vyrapol <129541586+vyrapol@users.noreply.github.com>
2023-09-08 14:53:34 +0700

    1 file modified

--- test/leaveplan.test.ts
+++ test/leaveplan.test.ts
@@ -1,5 +1,12 @@
 import { describe, it, expect } from 'vitest';
-let isCancel = true;
+let is_cancel = true;
+let is_activate = true;
+
+type Val = {
+  is_cancel: boolean;
+  is_activate: boolean;
+  start: string;
+};
 
 const requestType = {
   Normal: 'normal',
@@ -18,25 +25,84 @@ const isRevising = (request_type: string, route: string) => {
   return isTypeRevise || isPathRevise;
 };
 
+const shouldShowEnableDisable = (isApproved: boolean, isRevising: boolean) => {
+  return isApproved && isRevising;
+};
+
+const shouldEnableButton = (start: string) => new Date() < new Date(start);
+
+const showDisableButton = (val: Val, shouldShowEnableDisable) => {
+  if (!shouldEnableButton(val.start)) return false;
+  return shouldShowEnableDisable && !val.is_cancel && !val.is_activate;
+};
+const showEnableButton = (val: Val, shouldShowEnableDisable) => {
+  if (!shouldEnableButton(val.start)) return false;
+  return shouldShowEnableDisable && val.is_cancel && !val.is_activate;
+};
+
 describe('should show enable button', function () {
   it('should return true if status is approved while mode isnt adjustment and is cancel', () => {
-    const isEnable =
-      isApproved('approved') &&
+    const shouldShowEnableDisableButton = shouldShowEnableDisable(
+      isApproved('approved'),
       !isRevising(
         requestType.Normal,
         'http://127.0.0.1:3000/timeoff/plan-request/15'
-      ) &&
-      isCancel;
+      )
+    );
+    const val: Val = {
+      is_activate: false,
+      is_cancel: true,
+      start: '2023-09-25',
+    };
+    const isEnable = showEnableButton(val, shouldShowEnableDisableButton);
     expect(isEnable).toBeTruthy();
   });
   it('should return false if status is approved while mode isnt adjustment and is not cancel', () => {
-    const isEnable =
-      isApproved('draft') &&
+    const shouldShowEnableDisableButton = shouldShowEnableDisable(
+      isApproved('draft'),
       !isRevising(
         requestType.Revise,
         'http://127.0.0.1:3000/timeoff/plan-request/15/adjustment'
-      ) &&
-      !isCancel;
+      )
+    );
+    const val: Val = {
+      is_activate: true,
+      is_cancel: false,
+      start: '2023-08-25',
+    };
+    const isEnable = showDisableButton(val, shouldShowEnableDisableButton);
     expect(isEnable).toBeFalsy();
+  });
+  it('should hide button if date is smaller than current date', () => {
+    const shouldShowEnableDisableButton = shouldShowEnableDisable(
+      isApproved('draft'),
+      !isRevising(
+        requestType.Revise,
+        'http://127.0.0.1:3000/timeoff/plan-request/15/adjustment'
+      )
+    );
+    const val: Val = {
+      is_activate: false,
+      is_cancel: false,
+      start: '2023-02-13',
+    };
+    const isHidden = !showDisableButton(val, shouldShowEnableDisableButton);
+    expect(isHidden).toBeTruthy();
+  });
+  it('should show button if date is bigger than current date', () => {
+    const shouldShowEnableDisableButton = shouldShowEnableDisable(
+      isApproved('approved'),
+      !isRevising(
+        requestType.Normal,
+        'http://127.0.0.1:3000/timeoff/plan-request/15'
+      )
+    );
+    const val: Val = {
+      is_activate: false,
+      is_cancel: false,
+      start: '2024-01-01',
+    };
+    const isHidden = showDisableButton(val, shouldShowEnableDisableButton);
+    expect(isHidden).toBeTruthy();
   });
 });
